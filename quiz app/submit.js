695399053a63553f7de80e94d7f77c2d9ffef718
jithendra756 <machanijithendra756@gmail.com>
2025-11-06 00:42:13 +0530

    updated submit page and grades

--- quiz app/submit.js
+++ quiz app/submit.js
@@ -1,6 +1,9 @@
 const score = document.querySelector(".score");
-const selectedAnswers = localStorage.getItem("selectedAnswers");
+const grade = document.querySelector(".grade").querySelector("p");
+const selectedAnswers = JSON.parse(localStorage.getItem("selectedAnswers")) || [];
 let calculatedScore = 0;
+const grades = ["Outstanding","Excellent","Good","Satisfactory","Marginal","Unsatisfactory"];
+
 async function loadQuestions(){
     const res = await fetch("questions.json");
     const data = await res.json();
@@ -8,13 +11,28 @@ async function loadQuestions(){
 }
 
 async function calculateScore() {
+
     const questions = await loadQuestions();
-    // console.log(questions[0]);
+    
     for(let i=0;i<25;i++){
         if(selectedAnswers[i] === questions[i].answer)
             calculatedScore+=1;
     }
+
     score.textContent = calculatedScore;
+    if(calculatedScore>=23){
+        grade.textContent = grades[0]+"!";
+    }else if(calculatedScore>=19){
+        grade.textContent = grades[1]+"!";
+    }else if(calculatedScore>=15){
+        grade.textContent = grades[2]+"!";
+    }else if(calculatedScore>=11){
+        grade.textContent = grades[3]+"!";
+    }else if(calculatedScore>=6){
+        grade.textContent = grades[4]+"!";
+    }else{
+        grade.textContent = grades[5]+"!";
+    }
 }
 
 calculateScore();
